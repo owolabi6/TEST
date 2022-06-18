03877b85fea3b2b44df5d6d1d9e88d837842e3a2
owolabi6 <harunamalikowo@gmail.com>
2022-06-18 12:11:21 -0800

    second commit

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(String[] args) 
+        { 
+    
+        /*Console.WriteLine("Enter firstName:");
+          string firstName = Console.ReadLine();
+          Console.WriteLine("firstName is:" + firstName);
+          Console.WriteLine("Enter lastName:");
+          string lastName = Console.ReadLine();
+          Console.WriteLine("lastName is:" + lastName);
+          Console.WriteLine("Enter age");
+          int age =Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Enter favorite color:");
+          string favoritecolor = Console.ReadLine();
+          Console.WriteLine("favorite color is:" + favoritecolor);
+          Console.WriteLine("favorite food:");
+          string favoritefood = Console.ReadLine();
+          Console.WriteLine("favorite food is:" + favoritefood);
+          Console.WriteLine("Enter state of origin:");
+          string stateoforigin = Console.ReadLine();
+          Console.WriteLine("state of origin is:" + stateoforigin);
+
+
+          Console.Write("My name is" + firstName +lastName +" from " + stateoforigin);
+          Console.Write(". I am " + age + " years old. ");
+          Console.WriteLine(" My favorite food is " + favoritefood + " and my favorite color is " + favoritecolor);
+
+
+          Console.Write($"My name is {firstName} {lastName}  from  {stateoforigin}. \n I am {age} years old. \n My favorite food is {favoritefood} and my favorite color is {favoritecolor} ");*/
+
+           int age = 18;
+            Console.WriteLine(age==18);
+            Console.WriteLine(age <18);
+            Console.WriteLine(age<=18); 
+
+            Console.WriteLine("Input age:");
+            int ageOld = Convert.ToInt32(Console.ReadLine());
+            int age = 18;
+
+            if(ageOld < 18);
+            
+                Console.WriteLine("You are not eligible to vote");
+                Console.WriteLine("You are too old");    
+
+                /*int num = 12;
+                double num2 =num;
+                string mydouble = "78.98";
+                double num3 = Convert.ToDouble(mydouble);
+                int myInt = (int)num3;
+                Console.WriteLine(myInt);
+                Console.WriteLine(num3);
+
+                double cprice, sprice, plamt;
+
+                Console.WriteLine("\n\n");
+                Console.WriteLine("calculate profit and loss:\n");
+                Console.WriteLine("____________________________");
+                Console.WriteLine("\n\n");
+
+                Console.WriteLine("Input cost price: ");*/
+                
+            
+
+                /*int age = 18;
+                Console.WriteLine("You are NOT eligible to vote");
+                Console.WriteLine(age ==18);
+                Console.WriteLine("You are ELIGIBLE to vote");
+                Console.WriteLine(age <=18);
+
+                Console.WriteLine("Enter age:");
+                 age = Convert.ToInt32(Console.ReadLine());*/
+            }
+
+
+
+        }
+    }   
